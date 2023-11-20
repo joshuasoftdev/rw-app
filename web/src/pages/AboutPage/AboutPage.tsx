af161dfe89df2100712547de467b5c99f8a86f44
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2023-11-19 23:16:05 -0800

    commit 3

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -6,11 +6,13 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <h1>AboutPage</h1>
-      <p>
+      <h1 className="mb-4 flex justify-center font-serif text-6xl font-bold text-rose-500">
+        AboutPage
+      </h1>
+      <p className=" flex justify-center">
         Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
       </p>
-      <p>
+      <p className=" flex justify-center">
         My default route is named <code>about</code>, link to me with `
         <Link to={routes.about()}>About</Link>`
       </p>
